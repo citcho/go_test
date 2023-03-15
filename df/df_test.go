032393ba2f4f72491bd487caeccf194efb503c7a
kobayashi <citcho716@gmail.com>
2023-03-16 00:30:39 +0900

    test: df performance

---
+++ df/df_test.go
@@ -0,0 +1,72 @@
+package df_test
+
+import (
+	"bytes"
+	"context"
+	"io/ioutil"
+	"testing"
+
+	"github.com/go-gota/gota/dataframe"
+	"github.com/rocketlaunchr/dataframe-go/exports"
+	"github.com/rocketlaunchr/dataframe-go/imports"
+	"github.com/tobgu/qframe"
+)
+
+func BenchmarkQFrame(b *testing.B) {
+	bs, err := ioutil.ReadFile("iris.csv")
+	if err != nil {
+		b.Fatal(err)
+	}
+	b.ReportAllocs()
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		var out bytes.Buffer
+		qf := qframe.ReadCSV(bytes.NewBuffer(bs))
+		qf = qf.Select("sepal_length", "species")
+		err = qf.ToCSV(&out)
+		if err != nil {
+			b.Fatal(err)
+		}
+	}
+}
+
+func BenchmarkGota(b *testing.B) {
+	bs, err := ioutil.ReadFile("iris.csv")
+	if err != nil {
+		b.Fatal(err)
+	}
+	b.ReportAllocs()
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		var out bytes.Buffer
+		df := dataframe.ReadCSV(bytes.NewBuffer(bs))
+		df = df.Select([]string{"sepal_length", "species"})
+		err = df.WriteCSV(&out, dataframe.WriteHeader(true))
+		if err != nil {
+			b.Fatal(err)
+		}
+	}
+}
+
+func BenchmarkDataframeGo(b *testing.B) {
+	bs, err := ioutil.ReadFile("iris.csv")
+	if err != nil {
+		b.Fatal(err)
+	}
+	b.ReportAllocs()
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		var out bytes.Buffer
+		df, err := imports.LoadFromCSV(context.Background(), bytes.NewReader(bs))
+		if err != nil {
+			b.Fatal(err)
+		}
+		df.RemoveSeries("sepal_width")
+		df.RemoveSeries("petal_length")
+		df.RemoveSeries("petal_width")
+		err = exports.ExportToCSV(context.Background(), &out, df)
+		if err != nil {
+			b.Fatal(err)
+		}
+	}
+}
